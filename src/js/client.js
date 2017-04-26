671b06608c71a58afb1ac1ca0a9075443dc1ddb8
Mary Gao <maryqygao@gmail.com>
2017-04-26 00:40:11 -0700

    React Component - JS expression-wrapping in JSX

--- src/js/client.js
+++ src/js/client.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Layout extends React.Component {
+  constructor() {
+    super();
+    this.name = 'Mary';
+  }
+
   render() {
     return (
-      <h1>It's working!</h1>
+      <h1>It's {this.name}!</h1>
     );
   }
 }
