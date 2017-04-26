3a3ee0358b4ec0aa6e085003c1f7648c1c8a6f92
Mary Gao <maryqygao@gmail.com>
2017-04-26 03:39:43 -0700

    Add state in Layout component

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,9 +4,18 @@ import Footer from './Footer';
 import Header from './Header';
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = { name: 'Mary' };
+  }
+
   render() {
+    setTimeout(() => {
+      this.setState({ name: 'Maria' });
+    }, 1000);
     return (
       <div>
+        {this.state.name}
         <Header />
         <Footer />
       </div>
